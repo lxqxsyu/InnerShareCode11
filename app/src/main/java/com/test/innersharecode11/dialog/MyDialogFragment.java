e7c0c32178fab6356916889f3f2531d466d845a9
lxqxsyu <lxq_xsyu@163.com>
2019-09-30 09:01:21 +0800

    create project

---
+++ app/src/main/java/com/test/innersharecode11/dialog/MyDialogFragment.java
@@ -0,0 +1,14 @@
+package com.test.innersharecode11.dialog;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * 描述：
+ * 日期：2019/9/30
+ * 作者：水寒
+ * 邮箱：lxq_xsyu@163.com
+ */
+public class MyDialogFragment extends DialogFragment {
+
+
+}
